f566a682f25aa9f5fc003da56e70f358ef131509
Best Sg <bestsg.rns.2025@gmail.com>
2025-09-11 13:07:27 +0530

    change logo2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
-import logo from "../assets/logo1.png";
+import logo from "../assets/logo2.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
